765efccb122d81a2e92eb8acc1b8077c2451e227
Kyro Kohan <kyrokohan@gmail.com>
2025-08-23 21:04:50 -0700

    chore: add checksum generator to release builder script

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
 # Linux
-CGO_ENABLED=0 GOOS=linux   GOARCH=amd64 go build -o dist/golineart-linux-amd64 ./cmd/gla/main.go
-CGO_ENABLED=0 GOOS=linux   GOARCH=arm64 go build -o dist/golineart-linux-arm64 ./cmd/gla/main.go
+CGO_ENABLED=0 GOOS=linux   GOARCH=amd64 go build -o dist/golineart-linux-amd64 cmd/gla/main.go
+CGO_ENABLED=0 GOOS=linux   GOARCH=arm64 go build -o dist/golineart-linux-arm64 cmd/gla/main.go
+
 # macOS
-CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64 go build -o dist/golineart-darwin-amd64 ./cmd/gla/main.go
-CGO_ENABLED=0 GOOS=darwin  GOARCH=arm64 go build -o dist/golineart-darwin-arm64 ./cmd/gla/main.go
+CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64 go build -o dist/golineart-darwin-amd64 cmd/gla/main.go
+CGO_ENABLED=0 GOOS=darwin  GOARCH=arm64 go build -o dist/golineart-darwin-arm64 cmd/gla/main.go
+
 # Windows
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/golineart-windows-amd64.exe ./cmd/gla/main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o dist/golineart-windows-arm64.exe ./cmd/gla/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/golineart-windows-amd64.exe cmd/gla/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o dist/golineart-windows-arm64.exe cmd/gla/main.go
+
+# checksums
+shasum -a 256 dist/* > dist/SHA256SUMS.txt
